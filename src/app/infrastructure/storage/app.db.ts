f5aed6df47206f94060a543ad334e65f060c27e6
Alexander Bashenin <timebender00@gmail.com>
2025-02-19 20:55:02 +0400

    added dexie

---
+++ src/app/infrastructure/storage/app.db.ts
@@ -0,0 +1,58 @@
+import Dexie, { Table } from 'dexie';
+import { QuizSettings } from '../../domain/models/settings.models';
+import { WinnerStore } from '../../domain/models/winner.model';
+import { Category } from '../../domain/models/category.model';
+
+class AppDB extends Dexie {
+    settings!: Table<QuizSettings, string>;
+    categories!: Table<Category, number>;
+    winners!: Table<WinnerStore, string>;
+
+    constructor() {
+        super('ngdexieliveQuery');
+        this.version(1).stores({
+            settings: 'id',
+            categories: 'id',
+            winners: 'id',
+        });
+        this.on('populate', () => this.populate());
+    }
+
+    async populate() {
+        await db.settings.add({
+            id: 'settings',
+            playerName: 'Player',
+            difficulty: 'medium',
+            type: 'multiple',
+            category: null,
+        });
+        await db.winners.bulkAdd([
+            {
+                id: 'winners_easy',
+                value: [{ playerName: 'Player', score: 10, difficulty: 'easy', date: new Date(2025, 0, 1) }],
+            },
+            {
+                id: 'winners_medium',
+                value: [
+                    { playerName: 'fox', score: 30, difficulty: 'medium', date: new Date(2025, 0, 1) },
+                    { playerName: 'Player', score: 15, difficulty: 'medium', date: new Date(2025, 0, 1) },
+                ],
+            },
+            {
+                id: 'winners_hard',
+                value: [{ playerName: 'fox', score: 40, difficulty: 'hard', date: new Date(2025, 0, 1) }],
+            },
+        ]);
+    }
+
+    async resetDatabase() {
+        await db.transaction('rw', 'settings', 'winners', () => {
+            this.settings.clear();
+            this.categories.clear();
+            this.winners.clear();
+            this.populate();
+        });
+    }
+}
+
+export const db = new AppDB();
